986036911e1b490d11a4ae76a01edf6b6946a6b4
hanghae99 <didaltjs87@gmail.com>
2025-04-04 03:39:13 +0900

    swagger test

---
+++ src/main/java/kr/hhplus/be/server/test/SwaggerConfig.java
@@ -0,0 +1,17 @@
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Spring Boot REST API Specifications")
+                .description("Specification")
+                .version("1.0.0");
+    }
+}
